84bbe81ddfd71e025eedd0e5e3277d48e4f5a848
T-pixelt <180825126+T-pixelt@users.noreply.github.com>
2024-12-05 22:04:28 -0500

    Update env-vars-plugin.rb

--- _plugins/env-vars-plugin.rb
+++ _plugins/env-vars-plugin.rb
@@ -1,8 +1,6 @@
 module Jekyll
     class EnvVariables < Generator
-      def generate(site)
-        site.config['env'] = {}
-        ENV.each_pair { |k, v| site.config['env'][k] = v }
+      def ['env'][k] = v }
       end
     end
-  end
+  
